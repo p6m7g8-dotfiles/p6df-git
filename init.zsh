31717ae6c900bc7ddad6be737f475aca025f958b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-19 12:56:05 -0500

    chore(docs): regen

--- init.zsh
+++ init.zsh
@@ -79,7 +79,6 @@ p6df::modules::git::init() {
 #
 # Function: p6df::modules::git::aliases::init()
 #
-#  Environment:	 CPF CPSF
 #>
 ######################################################################
 p6df::modules::git::aliases::init() {
